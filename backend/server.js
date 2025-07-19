5483eb328457a62dc9d3892a5bd83b99da5293fc
ritikparmar45 <parmarritikk@gmail.com>
2025-07-19 19:36:10 +0530

    cors chagned

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://vsbs.vercel.app', 'http://localhost:5173'], // Allowed origins
+  origin: ['https://vehicle-app-seven.vercel.app', 'http://localhost:5173'], // Allowed origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
